030f35fef919cf1a06508f494c837de19d87a7ab
mh10445 <hussein@blockgemini.com>
2020-06-28 18:02:32 +0400

    Change Script

--- app.js
+++ app.js
@@ -3,27 +3,16 @@ const repoName = process.argv[4];
 const version = process.argv[3];
 const fs = require("fs");
 
-const repos = {
-  "node-react-blog-backend": {
-    version: 26,
-  },
-  "node-react-blog-frontend": {
-    version: 22,
-  },
-  "node-react-blog-nginx": {
-    version: 3,
-  },
-};
+const data = fs.readFileSync("versions.json", "utf-8");
+const parsedData = JSON.parse(data);
 
-for (let item in repos) {
+for (let item in parsedData) {
   if (item === repoName) {
     repos[item]["version"] = +version;
     fs.writeFileSync("versions.json", JSON.stringify(repos));
   }
 }
 
-const data = fs.readFileSync("versions.json", "utf-8");
-const parsedData = JSON.parse(data);
 shell.exec(
   `./deploy.sh 4001 ${parsedData["node-react-blog-backend"]["version"]} ${parsedData["node-react-blog-frontend"]["version"]} ${parsedData["node-react-blog-nginx"]["version"]}`
 );
